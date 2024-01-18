a785dc7758f9e0258730ac0aec844582eb5edfc7
tahiradmin <buat.loe@gmail.com>
2024-01-18 11:05:21 +0700

    Pengumpulan Tugas 3

---
+++ Tugas 3/Tugas-3A/looping.dart
@@ -0,0 +1,55 @@
+// Soal No. 1 Looping While 
+//Looping Pertama
+// void main(){
+//     var i = 1;
+//     while(i <= 20){
+//         i = i += 1;
+//         print(i.toString() + ' - I love coding');
+//         i++;
+//     }
+
+// }
+
+//Looping Kedua
+// void main(){
+//     var i = 22;
+//     while(i > 2 ){
+//         i = i -= 2 ;
+//         print(i.toString() + ' -  I will become a mobile developer');
+//     }
+// }
+
+//Soal No. 2 Looping menggunakan for
+// void main(){
+//     for(var i = 1; i<=20; i++){
+//     if (i % 3 == 0 && i % 2 == 1) {
+//         print(i.toString() + ' - I Love Coding');
+//         } else if (i % 2 == 1) {
+//         print(i.toString() + ' - Santai');
+//         } else {
+//         print(i.toString() + ' - Berkualitas');
+//         }
+//     }
+// }
+
+// No. 3 Membuat Persegi Panjang #
+// void main() {
+//   int row = 1;
+//   int totalRows = 4;
+
+//   while (row <= totalRows) {
+//     int column = 1;
+//     print('########');
+//     row++;
+//   }
+// }
+
+// No. 4 Membuat Tangga 
+// void main() {
+//   int totalRows = 8;
+//   for (int row = 1; row <= totalRows; row++) {
+//     for (int column = 1; column <= row; column++) {
+//       print('#');
+//     }
+//   }
+// }
---
+++ Tugas 3/Tugas-3B/function.dart
@@ -0,0 +1,37 @@
+// No. 1 
+// void main(){
+//     print(teriak());
+// }
+
+// teriak(){
+//     return "Halo Sanbers!";
+// }
+
+//No. 2 
+// void main(){
+//     var num1 = 12;
+//     var num2 = 4;
+//     print(kalikan(num1,num2));
+//   }
+
+// kalikan(num1, num2){
+//     return num1 * num2;
+ 
+// }
+
+//No. 3
+// void main(){
+// final String name = "Achmad";
+// final int age = 30;
+// final String address = "Jln. Malioboro, Yogyakarta";
+// final String hobby = "Gaming!";
+ 
+// print(introduce(name,age.toString(),address,hobby));
+
+// }
+
+// introduce(name, age, address, hobby){
+//   return "Nama saya " + name + ", umur saya " + age + " tahun, alamat saya di " + address + ", dan saya punya hobby yaitu " + hobby;
+// }
+
+
