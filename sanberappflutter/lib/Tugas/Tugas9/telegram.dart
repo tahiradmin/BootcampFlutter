1bd9e2f914f343f42e7d0e0330ba8018868dc6a0
tahiradmin <buat.loe@gmail.com>
2024-01-25 16:50:00 +0700

    Tugas 9

---
+++ sanberappflutter/lib/Tugas/Tugas9/telegram.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Telegram extends StatefulWidget {
+  const Telegram({super.key});
+
+  @override
+  State<Telegram> createState() => _TelegramState();
+}
+
+class _TelegramState extends State<Telegram> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
