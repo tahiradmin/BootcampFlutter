859ec4c2cbdf61d8122603a6247ef929b3ed6b40
tahiradmin <buat.loe@gmail.com>
2024-01-26 15:15:42 +0700

    Membuat Aplikasi Telegram

--- sanberappflutter/lib/lib/Tugas/Tugas9/Telegram.dart
+++ sanberappflutter/lib/lib/Tugas/Tugas9/Telegram.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sanberappflutter/lib/Tugas/Tugas9/drawerscreen.dart';
+import 'chart_model.dart';
 
 class Telegram extends StatefulWidget {
   const Telegram({super.key});
@@ -14,7 +15,12 @@ class _TelegramState extends State<Telegram> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Telegram"),
-        actions: const [],
+        actions: const <Widget>[
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Icon(Icons.search),
+          )
+        ],
       ),
       drawer: const DrawerScreen(),
       body: ListView.separated(
@@ -22,20 +28,25 @@ class _TelegramState extends State<Telegram> {
             return ListTile(
               leading: CircleAvatar(
                 radius: 28,
-                backgroundImage: NetworkImage(items[i].profileurl),
+                backgroundImage: NetworkImage(items[i].profileUrl!),
               ),
               title: Text(
-                items[i].name,
+                items[i].name!,
                 style: const TextStyle(fontWeight: FontWeight.bold),
               ),
-              subtitle: Text(items[i].message),
-              trailing: Text(items[i].time),
+              subtitle: Text(items[i].message!),
+              trailing: Text(items[i].time!),
             );
           },
           separatorBuilder: (ctx, i) {
             return const Divider();
           },
           itemCount: items.length),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: const Color(0xFF65a9e0),
+        onPressed: () {},
+        child: const Icon(Icons.create, color: Colors.white),
+      ),
     );
   }
 }
